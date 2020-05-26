04adc143396cdc5632bb5e86129c737c7f483133
Ubuntu <ubu@azure-japan.euuw0zymfk2erpb3nixufbniyg.lx.internal.cloudapp.net>
2020-05-26 14:58:04 +0000

    8-12

---
+++ ex11.py
@@ -0,0 +1,17 @@
+print("How old are you?", end='    ')
+age = input()
+print("How tall are you?", end='    ')
+height = input()
+print("How much do you weigh?", end='    ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
+
+print("How old are you?")
+age = input()
+print("How tall are you?")
+height = input()
+print("How much do you weigh?")
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
